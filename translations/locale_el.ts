d8db63231be607b1f64838ba2c6015d421c1532b
Joachim Schmitz <jojo@schmitz-digital.de>
2012-06-04 16:05:08 +0200

    adding more languages
    Danish, Norwegian, Swedish, Polish, Slovak, Czech (all same as German)
    and Greek

---
+++ translations/locale_el.ts
@@ -0,0 +1,192 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="el_GR">
+<context>
+    <name>notenames</name>
+    <message>
+        <location filename="../notenames.js" line="59"/>
+        <source>Fbb</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="60"/>
+        <source>Cbb</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="61"/>
+        <source>Gbb</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="62"/>
+        <source>Dbb</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="63"/>
+        <source>Abb</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="64"/>
+        <source>Ebb</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="65"/>
+        <source>Bbb</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="66"/>
+        <source>Fb</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="67"/>
+        <source>Cb</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="69"/>
+        <source>Gb</source>
+        <translation>Σολ ύφεση</translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="70"/>
+        <source>Db</source>
+        <translation>Ρε ύφεση</translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="71"/>
+        <source>Ab</source>
+        <translation>Λα ύφεση</translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="72"/>
+        <source>Eb</source>
+        <translation>Μι ύφεση</translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="73"/>
+        <source>Bb</source>
+        <translation>Σι ύφεση</translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="74"/>
+        <source>F</source>
+        <translation>Φα</translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="75"/>
+        <source>C</source>
+        <translation>Ντο</translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="76"/>
+        <source>G</source>
+        <translation>Σολ</translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="77"/>
+        <source>D</source>
+        <translation>Ρε</translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="78"/>
+        <source>A</source>
+        <translation>Λα</translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="79"/>
+        <source>E</source>
+        <translation>Μι</translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="80"/>
+        <source>B</source>
+        <translation>Σι</translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="82"/>
+        <source>F#</source>
+        <translation>Φα δίεση</translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="83"/>
+        <source>C#</source>
+        <translation>Ντο δίεση</translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="84"/>
+        <source>G#</source>
+        <translation>Σολ δίεση</translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="85"/>
+        <source>D#</source>
+        <translation>Ρε δίεση</translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="86"/>
+        <source>A#</source>
+        <translation>Λα δίεση</translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="87"/>
+        <source>E#</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="88"/>
+        <source>B#</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="89"/>
+        <source>F##</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="90"/>
+        <source>C##</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="91"/>
+        <source>G##</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="92"/>
+        <source>D##</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="93"/>
+        <source>A##</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="94"/>
+        <source>E##</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="95"/>
+        <source>B##</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="96"/>
+        <source>?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../notenames.js" line="112"/>
+        <source>Note Names</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
--- translations/lrelease.cmd
+++ translations/lrelease.cmd
@@ -1,2 +1,2 @@
-C:/QtSDK/Desktop/Qt/4.8.0/mingw/bin/lrelease locale_en.ts locale_de.ts locale_nl.ts locale_ja.ts locale_it.ts locale_fr.ts locale_es.ts locale_pt.ts locale_ru.ts locale_ro.ts
+C:/QtSDK/Desktop/Qt/4.8.0/mingw/bin/lrelease locale_en.ts locale_de.ts locale_nl.ts locale_ja.ts locale_it.ts locale_fr.ts locale_es.ts locale_pt.ts locale_ru.ts locale_ro.ts locale_el.ts locale_da.ts locale_nb.ts locale_sv.ts locale_pl.ts locale_sk.ts locale_cs.ts
 pause
