39ba83ae26f43b39b13936ff25e7a62155b06f91
Joachim Schmitz <jojo@schmitz-digital.de>
2016-01-12 13:00:46 +0100

    update helper scripts to Qt-5.4

--- translations/lupdate.cmd
+++ translations/lupdate.cmd
@@ -1,2 +1,2 @@
-C:/QtSDK/Desktop/Qt/5.3/mingw/bin/lupdate .. -no-obsolete -locations absolute -ts locale_en.ts locale_de.ts locale_nl.ts locale_ja.ts locale_it.ts locale_fr.ts locale_es.ts locale_pt.ts locale_ru.ts locale_ro.ts locale_el.ts locale_da.ts locale_nb.ts locale_sv.ts locale_pl.ts locale_sk.ts locale_cs.ts
+C:/Qt/5.4/mingw491_32/bin/lupdate .. -no-obsolete -locations absolute -ts locale_en.ts locale_de.ts locale_nl.ts locale_ja.ts locale_it.ts locale_fr.ts locale_es.ts locale_pt.ts locale_ru.ts locale_ro.ts locale_el.ts locale_da.ts locale_nb.ts locale_sv.ts locale_pl.ts locale_sk.ts locale_cs.ts
 pause
